9b92d3526e16ae8d624bca900519d8e247742fbb
quintuslabs <quintus.labs@gmail.com>
2018-10-05 11:09:33 +0530

    Volly Added

---
+++ app/src/main/java/com/quintus/labs/androidwithvolly/User.java
@@ -0,0 +1,37 @@
+package com.quintus.labs.androidwithvolly;
+
+import java.util.Date;
+
+/**
+ * Created by Abhi on 20 Jan 2018 020.
+ */
+
+public class User {
+    String username;
+    String fullName;
+    Date sessionExpiryDate;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public Date getSessionExpiryDate() {
+        return sessionExpiryDate;
+    }
+
+    public void setSessionExpiryDate(Date sessionExpiryDate) {
+        this.sessionExpiryDate = sessionExpiryDate;
+    }
+}
